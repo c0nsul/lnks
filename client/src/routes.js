ca226a8257b15bbf1e0382ba67283cf8e3c2a845
ConsuL <consul.developer@gmail.com>
2021-02-07 22:44:31 +0300

    link detail page

--- client/src/routes.js
+++ client/src/routes.js
@@ -15,7 +15,7 @@ export const useRoutes = isAuthenticated => {
                 <Route path="/create" exact>
                     <CreatePage />
                 </Route>
-                <Route path="/details/:id">
+                <Route path="/detail/:id">
                     <DetailPage />
                 </Route>
                 <Redirect to="/create" />
--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -3,17 +3,16 @@ const jwt = require('jsonwebtoken')
 //config configuration
 const config = require('config')
 
-
 module.exports = (req, res, next) => {
     if (req.method === 'OPTIONS'){
         return next()
     }
 
     try {
-        const token = req.headers.authorization.split(' ') //bearer token
+        const token = req.headers.authorization.split(' ')[1] //bearer token
 
         if (!token){
-            res.status(401).json({message: 'Not authenticated!'})
+            return res.status(401).json({message: 'No token authenticated!'})
         }
 
         const decoded = jwt.verify(token, config.get('jwtSecret'))
@@ -22,6 +21,6 @@ module.exports = (req, res, next) => {
         //app continue working
         next()
     } catch (e) {
-
+        res.status(401).json({message: 'Authentication is failure'})
     }
 }
