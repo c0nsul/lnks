92f2f9fc9fc8dc7110d0ed82ee6c389ac18a55b5
ConsuL <consul.developer@gmail.com>
2021-02-08 12:43:41 +0300

    link redirect

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ app.use(express.json({extended: true}))
 //app
 app.use('/api/auth', require('./routes/auth.routes'))
 app.use('/api/link', require('./routes/link.routes'))
+app.use('/t', require('./routes/redirect.routes'))
 
 const PORT = config.get('port') || 5000
 
